da13a3afa9c75f22685732c56bd0bdaf037e5d69
Nitin Rudrawar <nitin.rudrawar@gmail.com>
2021-04-27 22:17:50 +0100

    All Contollers and List page with navbar

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,39 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
+  # GET /restaurants
+  def index
+    @lists = List.all
+  end
+
+  # GET /lists/1
+  def show
+  end
+
+  # GET /lists/new
+  def new
+    @list = List.new
+  end
+
+  # POST /restaurants
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
